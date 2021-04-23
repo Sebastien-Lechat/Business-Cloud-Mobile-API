05c4f098cf833943e6936484b7915fb65d2b30a7
Sebastien-Lechat <seb_le@hotmail.fr>
2021-04-23 21:49:06 +0200

    fix notification route

--- src/helpers/notificationHelper.ts
+++ src/helpers/notificationHelper.ts
@@ -7,9 +7,9 @@ config();
 const serverKey = process.env.FCM_KEY as string;
 const fcm = new FCM(serverKey);
 
-export const sendNotificationToOne = async (titleText: string, bodyText: string, user: ClientI, category?: string) => {
+export const sendNotificationToOne = async (titleText: string, bodyText: string, user: ClientI, targetId: string, category: string) => {
     try {
-        await Notification.create({ title: titleText, message: bodyText, userId: user._id, category: category });
+        await Notification.create({ title: titleText, message: bodyText, userId: user._id, category: category, targetId: targetId });
 
         user.fcmDevice?.map((device) => {
             const message = {
@@ -21,16 +21,17 @@ export const sendNotificationToOne = async (titleText: string, bodyText: string,
             };
             fcm.send(message, (err: any) => {
                 if (err) console.log('Something has gone wrong!', err);
+                else console.log('send');
             });
         });
     } catch (err) {
-
+        console.log(err);
     }
 
 };
 
 // Envoi d'une notification à l'utilisateur
-// sendNotificationToOne('Nouvelle facture', 'Une nouvelle facture à été émise. Cliquez ici pour la consulter.', user.data, 'Facture');
-// sendNotificationToOne('Nouveau projet', 'Un nouveau projet à été créer. Cliquez ici pour le consulter.', user.data, 'Projet');
-// sendNotificationToOne('Nouveau message de xxxxxx', 'Vous avez reçu un nouveau message de xxxxxx. Cliquez ici pour le consulter.', user.data, 'Message');
+// sendNotificationToOne('Nouvelle facture', 'Une nouvelle facture à été émise. Cliquez ici pour la consulter.', user.data, '60717a18c8fca720cc84e1c9', 'Facture');
+// sendNotificationToOne('Nouveau projet', 'Un nouveau projet à été créer. Cliquez ici pour le consulter.', user.data, '6076bf7369652d5474692b0d', 'Projet');
+// sendNotificationToOne('Nouveau message de xxxxxx', 'Vous avez reçu un nouveau message de xxxxxx. Cliquez ici pour le consulter.', user.data, 'xxxxx', 'Message');
 
