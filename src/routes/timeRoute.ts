be3abd30c4a4cee4d81a4c911e121fe8b89f073a
Sebastien-Lechat <seb_le@hotmail.fr>
2021-05-17 15:45:54 +0200

    change time rouite

--- src/routes/timeRoute.ts
+++ src/routes/timeRoute.ts
@@ -5,8 +5,9 @@ import { authMiddleware } from '../middlewares/authMiddleware';
 
 const route: express.Application = express();
 
-route.get('/times/projectId', [authMiddleware], TimeController.getTimesList);
+route.get('/times/:projectId', [authMiddleware], TimeController.getTimesList);
 route.post('/time', [authMiddleware], TimeController.create);
+route.put('/time', [authMiddleware], TimeController.update);
 route.delete('/time/:id', [authMiddleware], TimeController.delete);
 
 export { route as timeRouter };
